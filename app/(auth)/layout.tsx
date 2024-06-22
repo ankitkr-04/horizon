ef434842c9e55516b1f2324dc1b9b4aac1bfafd6
Ankit Kumar <ak0182274@gmail.com>
2024-06-22 12:17:46 +0530

    some layout chnages to auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,18 @@
+import Image from "next/image";
+
 export default function AuthLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <main>
+        <main className="min-h-screen flex w-full justify-between font-inter">
             {children}
+            <div className="auth-asset">
+                <div>
+                    <Image src="/icons/auth-image.svg" alt="Auth Image" width={500} height={500} />
+                </div>
+            </div>
         </main>
     );
 }
