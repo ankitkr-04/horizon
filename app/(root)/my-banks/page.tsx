543c3e5136b3fdb87bd0209b0296d7065abf8b8e
Ankit Kumar <ak0182274@gmail.com>
2024-06-29 10:05:21 +0530

    implement my bank form

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,8 +1,36 @@
+import BankCard from '@/components/BankCard';
+import HeaderBox from '@/components/HeaderBox'
+import { getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
-const MyBanks = () => {
+const MyBanks = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn!.$id })
+  // console.log(accounts);
+  
   return (
-    <div>MyBanks</div>
+    <section className="flex">
+      <div className="my-banks">
+        <HeaderBox title="My Banks Accounts" subtext='Effortlessly manage banking Activities' />
+
+        <div className="space-y-4">
+          <h2 className="header-2">Your Cards</h2>
+          <div className="flex flex-wrap gap-6">
+            {accounts && accounts.data.map((account: any) => (
+              <BankCard 
+                key={account.id}
+                account={account}
+                showBalance={true}
+                userName={`${loggedIn?.firstName} ${loggedIn?.lastName}` || 'Guest'}
+
+              />
+
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,11 +2,12 @@ import { formatAmount } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import CopyButton from './Copy'
 
 const BankCard = ({ account, userName, showBalance, }: CreditCardProps) => {
     return (
         <div className='flex flex-col'>
-            <Link href='/' className='bank-card'>
+            <Link href={`/transaction-history/?id=${account.id}`} className='bank-card '>
                 <div className='bank-card_content'>
                     <div>
                         <h1 className='text-16 font-semibold text-white'>
@@ -23,11 +24,11 @@ const BankCard = ({ account, userName, showBalance, }: CreditCardProps) => {
                                 {userName}
                             </h1>
                             <h2 className='text-12 font-semibold text-white'>
-                            ●●/●●
+                                ●●/●●
                             </h2>
                         </div>
                         <p className='text-14 font-semibold tracking-[1.1px] text-white'>
-                        ●●●● ●●●● ●●●● <span className='text-16'>{account.mask}</span>
+                            ●●●● ●●●● ●●●● <span className='text-16'>{account.mask}</span>
                         </p>
                     </article>
                 </div>
@@ -56,6 +57,7 @@ const BankCard = ({ account, userName, showBalance, }: CreditCardProps) => {
 
 
             </Link>
+            {showBalance && <CopyButton title={account?.shareableId} />}
 
         </div>
     )
